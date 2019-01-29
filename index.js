84ae3b61461a453fb46e3a233e974cd09bbef64c
Steven Mezzacappa <steven.mezzacappa@gmail.com>
2019-01-29 12:58:17 -0500

    message

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const PORT = 3000;
 app.use(express.static('public'));
 app.use(express.static('images'));
 app.use(express.static('favocmatic'));
-app.use(express.static('index.html'));
+
 app.listen(PORT, () => console.log(`app listening on ${PORT}`));
